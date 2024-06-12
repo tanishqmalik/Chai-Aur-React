aec1753aa34954fa82567e2d2886cc452b4dbce9
tanishqmalik <tanishqvideo@gmail.com>
2024-06-12 13:17:16 +0530

    okay

--- 01CAR/src/main.jsx
+++ 01CAR/src/main.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+
+function Hello(){
+    return(
+        <>
+        <h1>hello bahiya</h1>
+        </>
+        
+    )
+}
+
+
+const ReactElement =  {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const  anotherEle = (
+    return <reactElement/>;
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+        // Hello()
+        <Hello/>
+        anotherEle()
 )
