7cdbd786a813bfbddd5c7ca6136cfaf29fe65263
tanishqmalik <tanishqvideo@gmail.com>
2024-06-25 12:56:19 +0530

    mm

--- 04bgChanger/src/Pages/Home/Home.jsx
+++ 04bgChanger/src/Pages/Home/Home.jsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 
 function Home() {
     const [Color, setColor] = useState("olive")
+
+    // const settingColor =()=>{
+    //     const Color = backgroundColor.innerHTML
+    //     setColor(Color)
+    // }
     return (
         <div className="w-full h-screen duration-200"
             style={{ backgroundColor: Color }}
@@ -10,8 +15,20 @@ function Home() {
 
             <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
                 <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-                    <button className='outline-none px-4 rounded-full'
-                    style={{backgroundColor: "red"}}
+                    <button onClick={()=>{setColor("red")}} className='outline-none px-4 rounded-full text-white shadow-lg'
+                        style={{ backgroundColor: "red" }}
+                    >Red</button>
+
+                    <button onClick={()=>{setColor("blue")}} className='outline-none px-4 rounded-full text-white shadow-lg'
+                        style={{ backgroundColor: "blue" }}
+                    >Red</button>
+
+                    <button onClick={()=>{setColor("green")}} className='outline-none px-4 rounded-full text-white shadow-lg'
+                        style={{ backgroundColor: "green" }}
+                    >Red</button>
+
+                    <button onClick={()=>{setColor("white")}} className=' border border-zinc-900 px-4 rounded-full text-black shadow-lg'
+                        style={{ backgroundColor: "white" }}
                     >Red</button>
                 </div>
             </div>
